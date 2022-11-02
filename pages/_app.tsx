16a96f5140f6f40098181d9ad979ac35e3d247ee
Richard <richard@stromer.org>
2022-11-02 14:32:23 -0700

    install coreui css files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import "../styles/globals.css";
+import "@coreui/coreui/dist/css/coreui.min.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
