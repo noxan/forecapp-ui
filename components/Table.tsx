41471ab7e15dbfa3679b31d7806cdc861a8bb1f7
Richard <richard@stromer.org>
2022-11-02 13:49:45 -0700

    refactor table handling

---
+++ components/Table.tsx
@@ -0,0 +1,103 @@
+import { useReducer, useState } from "react";
+
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+const columnHelper = createColumnHelper<Person>();
+
+// const columns = [].map(columnHelper.createColumn);
+
+const columns = [
+  columnHelper.accessor("firstName", {
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor((row) => row.lastName, {
+    id: "lastName",
+    cell: (info) => <i>{info.getValue()}</i>,
+    header: () => <span>Last Name</span>,
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("age", {
+    header: () => "Age",
+    cell: (info) => info.renderValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("visits", {
+    header: () => <span>Visits</span>,
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("status", {
+    header: "Status",
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("progress", {
+    header: "Profile Progress",
+    footer: (info) => info.column.id,
+  }),
+];
+
+export default function Table({ defaultData }: { defaultData: any }) {
+  const [data, setData] = useState(() => [...defaultData]);
+  const rerender = useReducer(() => ({}), {})[1];
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div className="p-2">
+      <table>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          {table.getFooterGroups().map((footerGroup) => (
+            <tr key={footerGroup.id}>
+              {footerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.footer,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </table>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,57 @@
 import useSWR from "swr";
 import { parse as parseCSV } from "csv-parse/sync";
+import Table from "../components/Table";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.text());
 
-export default function Home() {
-  const { data, error } = useSWR("/datasets/air_passengers.csv", fetcher);
+type Person = {
+  firstName: string;
+  lastName: string;
+  age: number;
+  visits: number;
+  status: string;
+  progress: number;
+};
 
-  if (error || !data) {
-    return <div>Error: {JSON.stringify(error)}</div>;
+const defaultData: Person[] = [
+  {
+    firstName: "tanner",
+    lastName: "linsley",
+    age: 24,
+    visits: 100,
+    status: "In Relationship",
+    progress: 50,
+  },
+  {
+    firstName: "tandy",
+    lastName: "miller",
+    age: 40,
+    visits: 40,
+    status: "Single",
+    progress: 80,
+  },
+  {
+    firstName: "joe",
+    lastName: "dirte",
+    age: 45,
+    visits: 20,
+    status: "Complicated",
+    progress: 10,
+  },
+];
+
+export default function Home() {
+  const response = useSWR("/datasets/air_passengers.csv", fetcher);
+
+  if (response.error || !response.data) {
+    return <div>Error: {JSON.stringify(response.error)}</div>;
   }
 
-  const records = parseCSV(data);
+  const records = parseCSV(response.data);
 
-  console.log(records, error);
-
-  return <div>Hello world: {JSON.stringify(records)}</div>;
+  return (
+    <div>
+      <Table defaultData={defaultData} />
+    </div>
+  );
 }
