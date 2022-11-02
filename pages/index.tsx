a943b9b987b2dd9166df8f15d79c3a6c3867367d
Richard <richard@stromer.org>
2022-11-02 09:49:11 -0700

    load dataset and preview result

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,15 @@
+import useSWR from "swr";
+
+const fetcher = (url: string) => fetch(url).then((res) => res.text());
+
 export default function Home() {
-  return <div>Hello world</div>;
+  const { data, error } = useSWR("/datasets/air_passengers.csv", fetcher);
+
+  if (error) {
+    return <div>Error: {JSON.stringify(error)}</div>;
+  }
+
+  console.log(data, error);
+
+  return <div>Hello world: {JSON.stringify(data)}</div>;
 }
