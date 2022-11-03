d0f6b792c2858c7504a8ed0ec939b237860a606b
Richard <richard@stromer.org>
2022-11-03 14:45:20 -0700

    hide advanced dataset for now

---
+++ pages/dataset-experiment.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import * as danfo from "danfojs";
+
+import Table from "../components/Table";
+
+export default function Dataset() {
+  console.log("Dataset");
+  const [dataset, setDataset] = useState<undefined | danfo.DataFrame>();
+
+  useEffect(() => {
+    async function fetchData() {
+      const df_1 = await danfo.readCSV("/datasets/energy_dataset_small.csv");
+
+      const df = df_1.setIndex({ column: "time" });
+
+      setDataset(df);
+
+      const columns2 = df.columns.filter((col) => col !== "time");
+
+      df.plot("plot_div").line({ config: { columns: columns2 } });
+    }
+    console.log("useEffect");
+    if (!dataset) {
+      fetchData();
+    }
+  });
+
+  if (!dataset) {
+    return (
+      <div>
+        <div id="plot_div"></div>Loading
+      </div>
+    );
+  }
+
+  console.log(dataset);
+
+  return (
+    <div>
+      <div id="plot_div"></div>
+      <Table initialData={dataset} />
+    </div>
+  );
+}
+
+// Dataset.getInitialProps = async () => {
+//   console.log("getInitialProps");
+//   if (typeof window !== "undefined") {
+//     const danfo = await import("danfojs");
+//     const df = await danfo.readCSV("/datasets/energy_dataset.csv");
+//     return { df };
+//   } else {
+//     const danfo = await import("danfojs-node");
+//     const df = await danfo.readCSV("public/datasets/energy_dataset.csv");
+//     return { df: danfo.toJSON(df, { format: "row" }) };
+//   }
+// };
