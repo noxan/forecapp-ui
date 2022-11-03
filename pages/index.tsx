e8fc7e778490efd0e8dc0e6c8f465bd115f1611d
Richard <richard@stromer.org>
2022-11-03 15:38:59 -0700

    finish layout for start page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import {
   CCard,
   CCardBody,
+  CCardImage,
   CCardText,
   CCardTitle,
   CCol,
@@ -12,12 +13,21 @@ import Layout from "../components/Layout";
 type ExampleDataset = {
   title: string;
   filename: string;
+  image: string;
 };
 
 const datasetBaseUrl = "/public/datasets/";
 const datasetExamples: ExampleDataset[] = [
-  { title: "Energy prices", filename: "energy_dataset_small.csv" },
-  { title: "Air passengers", filename: "air_passengers.csv" },
+  {
+    title: "Energy prices",
+    filename: "energy_dataset_small.csv",
+    image: "photo-1473341304170-971dccb5ac1e",
+  },
+  {
+    title: "Air passengers",
+    filename: "air_passengers.csv",
+    image: "photo-1569629743817-70d8db6c323b",
+  },
 ];
 
 export default function Home() {
@@ -25,23 +35,26 @@ export default function Home() {
     <Layout>
       <CContainer>
         <h1>Home</h1>
-        <CContainer fluid>
-          <CRow>
-            {datasetExamples.map((dataset) => (
-              <CCol sm="auto" key={dataset.filename}>
-                <CCard>
-                  <CCardBody>
-                    <CCardTitle>{dataset.title}</CCardTitle>
-                    <CCardText>
-                      {datasetBaseUrl}
-                      {dataset.filename}
-                    </CCardText>
-                  </CCardBody>
-                </CCard>
-              </CCol>
-            ))}
-          </CRow>
-        </CContainer>
+        <CRow xs={{ cols: 1, gutter: 4 }} md={{ cols: 3 }}>
+          {datasetExamples.map((dataset) => (
+            <CCol xs key={dataset.filename}>
+              <CCard className="h-100">
+                <CCardImage
+                  orientation="top"
+                  src={`https://images.unsplash.com/${dataset.image}?fit=crop&w=640&q=80`}
+                />
+                <CCardBody>
+                  <CCardTitle>{dataset.title}</CCardTitle>
+                  <CCardText>
+                    {datasetBaseUrl}
+                    {dataset.filename}
+                  </CCardText>
+                </CCardBody>
+              </CCard>
+            </CCol>
+          ))}
+        </CRow>
+        File upload and selector to load custom datasets.
       </CContainer>
     </Layout>
   );
