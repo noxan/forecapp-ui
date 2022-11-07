8fe34b6e128816bd36b5160a13da406e03765e22
Richard <richard@stromer.org>
2022-11-07 10:51:45 -0800

    mock transforms store

---
+++ src/store/transforms.ts
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const transformSlice = createSlice({
+  name: "transforms",
+  initialState: undefined,
+  reducers: {
+    addTransform: () => undefined,
+    editTransform: () => undefined,
+    removeTransform: () => undefined,
+  },
+});
+
+export const { addTransform, editTransform, removeTransform } =
+  transformSlice.actions;
+
+export default transformSlice.reducer;
