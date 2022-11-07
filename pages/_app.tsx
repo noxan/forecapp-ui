c8ae025ab6f0f45faa306b76753683ee55557869
Richard <richard@stromer.org>
2022-11-07 15:01:54 -0800

    setup and use redux-persist

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import "../styles/globals.sass";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 import { store } from "../src/store";
+
+const persistor = persistStore(store);
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />;
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />;
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import datasets from "./datasets";
 import transforms from "./transforms";
 import models from "./models";
 
+const reducers = combineReducers({
+  datasets,
+  transforms,
+  models,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducers = persistReducer(persistConfig, reducers);
+
 export const store = configureStore({
-  reducer: {
-    datasets,
-    transforms,
-    models,
-  },
+  reducer: persistedReducers,
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
