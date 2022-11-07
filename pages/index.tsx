1026ffc6b1d8cf60021f89d404e1f864ddd0b836
Richard <richard@stromer.org>
2022-11-07 15:27:01 -0800

    get a chart working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
+// import "chart.js/auto";
 import { CButton, CCol, CContainer, CRow } from "@coreui/react";
 import { useSelector, useDispatch } from "react-redux";
+import { CChart } from "@coreui/react-chartjs";
 import { AppDispatch } from "../src/store";
 import { importDataset } from "../src/store/datasets";
 import Layout from "../components/Layout";
+import { useRef } from "react";
 
 const exampleDatasets = [
   "datasets/energy_dataset_small.csv",
@@ -10,9 +13,30 @@ const exampleDatasets = [
 ];
 
 export default function Home() {
+  const ref = useRef();
   const datasets = useSelector((state: any) => state.datasets);
   const isLoading = datasets.status === "loading";
   const dispatch = useDispatch<AppDispatch>();
+
+  const data = {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    datasets: [
+      {
+        label: "First dataset",
+        data: [33, 53, 85, 41, 44, 65],
+        fill: true,
+        backgroundColor: "rgba(75,192,192,0.2)",
+        borderColor: "rgba(75,192,192,1)",
+      },
+      {
+        label: "Second dataset",
+        data: [33, 25, 35, 51, 54, 76],
+        fill: false,
+        borderColor: "#742774",
+      },
+    ],
+  };
+
   return (
     <Layout>
       <CContainer>
@@ -31,8 +55,6 @@ export default function Home() {
             </CCol>
           ))}
         </CRow>
-      </CContainer>
-      <CContainer>
         <CRow>
           <CCol>{datasets.status}</CCol>
           {datasets.raw && (
@@ -40,6 +62,12 @@ export default function Home() {
               <pre>{JSON.stringify(datasets.raw.slice(0, 5))}</pre>
             </CCol>
           )}
+        </CRow>
+      </CContainer>
+      <CContainer>
+        <CRow className="my-2">
+          <h1>Visualization</h1>
+          <CChart data={data} type={"line"} ref={ref} />
         </CRow>
       </CContainer>
     </Layout>
