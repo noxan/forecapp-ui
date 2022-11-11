69f3b21f1b6da4f93706452dcacee33245baf2f0
Richard <richard@stromer.org>
2022-11-10 20:04:24 -0800

    add proxy for production api

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api-production/:slug",
+        destination:
+          "http://ec2-54-219-210-155.us-west-1.compute.amazonaws.com/:slug",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
