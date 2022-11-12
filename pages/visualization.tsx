5fc15947c30a43f6b6340140fef3406d5cfd2c7a
Richard <richard@stromer.org>
2022-11-11 18:27:03 -0800

    build visualization with chartjs

--- pages/visualization.tsx
+++ pages/visualization.tsx
@@ -1,7 +1,48 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
 import { CCol, CContainer, CRow } from "@coreui/react";
 import Link from "next/link";
 import Layout from "../components/Layout";
 import { useAppSelector } from "../src/hooks";
+import { capitalize } from "../src/helpers";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const transformDatasetForChart = (dataset: any[]) => {
+  const headers = Object.keys(dataset[0]).splice(1);
+
+  // TODO: remove hard coded "time" index
+  const timeLabels = dataset.slice(1).map((item) => item.time);
+
+  // console.log(headers);
+
+  return {
+    labels: timeLabels,
+    datasets: headers.map((header) => ({
+      label: capitalize(header),
+      data: dataset.map((item) => item[header]),
+      fill: false,
+      backgroundColor: "#" + Math.floor(Math.random() * 16777215).toString(16),
+    })),
+  };
+};
 
 export default function Visualization() {
   const datasets = useAppSelector((state) => state.datasets);
@@ -14,6 +55,10 @@ export default function Visualization() {
     );
   }
 
+  const chartData = transformDatasetForChart(datasets.raw.slice(0, 100));
+  // console.log(chartData.datasets);
+  console.log(chartData.datasets);
+
   return (
     <Layout>
       <CContainer>
@@ -24,7 +69,7 @@ export default function Visualization() {
         </CRow>
         <CRow className="my-2">
           <CCol>
-            <h1></h1>
+            <Line data={chartData} />
           </CCol>
         </CRow>
       </CContainer>
