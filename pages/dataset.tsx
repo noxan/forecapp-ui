61771c1277677c812835045e38e66f6433ae2e79
Richard <richard@stromer.org>
2022-11-11 17:50:14 -0800

    start building dataset page

--- pages/dataset.tsx
+++ pages/dataset.tsx
@@ -1,11 +1,30 @@
-import { CContainer } from "@coreui/react";
-import Navigation from "../components/Navigation";
+import { CCol, CContainer, CRow } from "@coreui/react";
+import Link from "next/link";
+import Layout from "../components/Layout";
+import Table from "../components/Table";
+import { useAppSelector } from "../src/hooks";
 
 export default function Dataset() {
+  const datasets = useAppSelector((state) => state.datasets);
+
+  if (!datasets) {
+    return (
+      <Layout>
+        <Link href="/">Import dataset first</Link>
+      </Layout>
+    );
+  }
+
   return (
-    <div>
-      <Navigation />
-      <CContainer>Dataset</CContainer>
-    </div>
+    <Layout>
+      <CContainer>
+        <CRow className="my-2">
+          <CCol>
+            <h1>Dataset</h1>
+          </CCol>
+        </CRow>
+      </CContainer>
+      {datasets.raw && <Table data={datasets.raw.slice(0, 3)} />}
+    </Layout>
   );
 }
