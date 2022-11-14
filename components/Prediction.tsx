7eb361d9f15cc5ca4b3882838545102830c095fc
Richard <richard@stromer.org>
2022-11-14 11:40:39 -0800

    make forecast chart full width

--- components/Prediction.tsx
+++ components/Prediction.tsx
@@ -33,90 +33,94 @@ const Prediction = ({
     : undefined;
 
   return (
-    <CContainer>
-      <CRow className="my-2">
-        <CCol>
-          <h1>Model input</h1>
-        </CCol>
-        <CCol>
-          <CButton onClick={() => setShowDebug(!showDebug)} color="link">
-            Toggle debug info
-          </CButton>
-        </CCol>
-      </CRow>
-      <CCollapse visible={showDebug}>
-        <CRow md={{ cols: 2 }}>
+    <>
+      <CContainer>
+        <CRow className="my-2">
           <CCol>
-            <h3>Dataset</h3>
-            <pre style={{ maxHeight: "20rem" }}>
-              {JSON.stringify(finalDataset, null, 2)}
-            </pre>
+            <h1>Model input</h1>
           </CCol>
           <CCol>
-            <h3>Parameters</h3>
-            <pre style={{ maxHeight: "20rem" }}>
-              {JSON.stringify(modelConfiguration, null, 2)}
-            </pre>
+            <CButton onClick={() => setShowDebug(!showDebug)} color="link">
+              Toggle debug info
+            </CButton>
           </CCol>
         </CRow>
-      </CCollapse>
-      <CRow className="my-2">
-        <CCol>
-          <CButton
-            disabled={status === "loading"}
-            className={`btn-loading${
-              status === "loading" ? " is-loading" : ""
-            }`}
-            onClick={() => {
-              dispatch(
-                neuralprophet({
-                  dataset: finalDataset,
-                  columns,
-                  configuration: modelConfiguration,
-                })
-              );
-            }}
-          >
-            {status === "loading" ? (
-              <>
-                <span
-                  className="btn-loading-spinner spinner-border spinner-border-sm"
-                  role="status"
-                  aria-hidden="true"
-                ></span>{" "}
-              </>
-            ) : (
-              ""
-            )}
-            Run prediction
-          </CButton>
-        </CCol>
-        <CCol>
-          <CBadge color="dark">{process.env.NEXT_PUBLIC_API_URL}</CBadge>
-        </CCol>
-      </CRow>
+        <CCollapse visible={showDebug}>
+          <CRow md={{ cols: 2 }}>
+            <CCol>
+              <h3>Dataset</h3>
+              <pre style={{ maxHeight: "20rem" }}>
+                {JSON.stringify(finalDataset, null, 2)}
+              </pre>
+            </CCol>
+            <CCol>
+              <h3>Parameters</h3>
+              <pre style={{ maxHeight: "20rem" }}>
+                {JSON.stringify(modelConfiguration, null, 2)}
+              </pre>
+            </CCol>
+          </CRow>
+        </CCollapse>
+        <CRow className="my-2">
+          <CCol>
+            <CButton
+              disabled={status === "loading"}
+              className={`btn-loading${
+                status === "loading" ? " is-loading" : ""
+              }`}
+              onClick={() => {
+                dispatch(
+                  neuralprophet({
+                    dataset: finalDataset,
+                    columns,
+                    configuration: modelConfiguration,
+                  })
+                );
+              }}
+            >
+              {status === "loading" ? (
+                <>
+                  <span
+                    className="btn-loading-spinner spinner-border spinner-border-sm"
+                    role="status"
+                    aria-hidden="true"
+                  ></span>{" "}
+                </>
+              ) : (
+                ""
+              )}
+              Run prediction
+            </CButton>
+          </CCol>
+          <CCol>
+            <CBadge color="dark">{process.env.NEXT_PUBLIC_API_URL}</CBadge>
+          </CCol>
+        </CRow>
+      </CContainer>
       {prediction && chartData && (
         <>
           <CChartLine type="line" data={chartData} />
           <CCollapse visible={showDebug}>
-            <CRow md={{ cols: 2 }}>
-              <CCol>
-                <h3>Forecast</h3>
-                <pre style={{ maxHeight: "20rem" }}>
-                  {JSON.stringify(prediction.forecast, null, 2)}
-                </pre>
-              </CCol>
-              <CCol>
-                <h3>Metrics</h3>
-                <pre style={{ maxHeight: "20rem" }}>
-                  {JSON.stringify(prediction.metrics, null, 2)}
-                </pre>
-              </CCol>
-            </CRow>
+            <CContainer>
+              <CRow md={{ cols: 2 }}>
+                <CCol>
+                  <h3>Forecast</h3>
+                  <pre style={{ maxHeight: "20rem" }}>
+                    {JSON.stringify(prediction.forecast, null, 2)}
+                  </pre>
+                </CCol>
+                <CCol>
+                  <h3>Metrics</h3>
+                  <pre style={{ maxHeight: "20rem" }}>
+                    {JSON.stringify(prediction.metrics, null, 2)}
+                  </pre>
+                </CCol>
+              </CRow>
+            </CContainer>
           </CCollapse>
         </>
       )}
-    </CContainer>
+    </>
   );
 };
 
