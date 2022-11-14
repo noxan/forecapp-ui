c3aecb7e4eda5c3a374d14c27c15f45c4713107c
Richard <richard@stromer.org>
2022-11-14 15:24:00 -0800

    add list of column headers

--- pages/dataset.tsx
+++ pages/dataset.tsx
@@ -1,11 +1,23 @@
-import { CCol, CContainer, CRow } from "@coreui/react";
+import {
+  CCol,
+  CContainer,
+  CNav,
+  CNavItem,
+  CNavLink,
+  CRow,
+  CTabContent,
+  CTabPane,
+} from "@coreui/react";
 import Link from "next/link";
+import { useState } from "react";
 import Layout from "../components/Layout";
 import Table from "../components/Table";
+import { capitalize } from "../src/helpers";
 import { useAppSelector } from "../src/hooks";
 
 export default function Dataset() {
   const datasets = useAppSelector((state) => state.datasets);
+  const [activeKey, setActiveKey] = useState(0);
 
   if (!datasets) {
     return (
@@ -14,6 +26,10 @@ export default function Dataset() {
       </Layout>
     );
   }
+
+  const dataset = datasets.raw;
+  // TODO: initialize empty header columns if dataset does not provide any
+  const headers = Object.keys(dataset[0]);
 
   return (
     <Layout>
@@ -29,6 +45,37 @@ export default function Dataset() {
         <CRow className="my-2">
           <CCol>
             <h2>Validation</h2>
+          </CCol>
+        </CRow>
+        <CRow>
+          <CCol md={3}>
+            <CNav variant="pills" className="flex-column">
+              {headers.map((header, index) => (
+                <CNavItem key={index}>
+                  <CNavLink
+                    href="javascript:void(0);"
+                    active={activeKey === index}
+                    onClick={() => setActiveKey(index)}
+                  >
+                    {capitalize(header)}
+                  </CNavLink>
+                </CNavItem>
+              ))}
+            </CNav>
+          </CCol>
+          <CCol>
+            <CTabContent>
+              {headers.map((header, index) => (
+                <CTabPane
+                  key={index}
+                  role="tabpanel"
+                  aria-labelledby={`${header}-tab`}
+                  visible={activeKey === index}
+                >
+                  {capitalize(header)}
+                </CTabPane>
+              ))}
+            </CTabContent>
           </CCol>
         </CRow>
       </CContainer>
