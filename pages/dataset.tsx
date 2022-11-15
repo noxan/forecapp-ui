c7e749116ac508a77a444d0cfe233c698223a953
Richard <richard@stromer.org>
2022-11-15 15:55:39 -0800

    improve layout and add statistics

--- pages/dataset.tsx
+++ pages/dataset.tsx
@@ -72,7 +72,8 @@ export default function Dataset() {
       <CContainer>
         <CRow className="my-2">
           <CCol>
-            <h2>Validation</h2>
+            {" "}
+            <h2>Primary time series column</h2>
           </CCol>
         </CRow>
         <CRow className="my-2">
@@ -93,121 +94,135 @@ export default function Dataset() {
               ]}
             />
           </CCol>
+          <CCol>
+            <div>{dataset.length} entries</div>
+            <div>{headers.length} columns</div>
+          </CCol>
         </CRow>
         {timeColumn !== SELECT_STATE_NONE &&
           timeColumn !== SELECT_STATE_INITIALIZE && (
-            <CRow className="my-2">
-              <CCol md={3}>
-                <CNav variant="pills" className="flex-column">
-                  {headers.map((header, index) => (
-                    <CNavItem key={index}>
-                      <CNavLink
-                        href={`#${header}`}
-                        active={activeKey === index}
-                        onClick={(evt) => {
-                          evt.preventDefault();
-                          setActiveKey(index);
-                        }}
-                      >
-                        {capitalize(header)}
-                      </CNavLink>
-                    </CNavItem>
-                  ))}
-                </CNav>
-              </CCol>
-              <CCol md={9}>
-                <CTabContent>
-                  {(() => {
-                    const column = headers[activeKey];
+            <>
+              <hr />
+              <CRow className="my-2">
+                <CCol>
+                  <h2>Columns explorer and configurations</h2>
+                </CCol>
+              </CRow>
+              <CRow className="my-2">
+                <CCol md={3}>
+                  <CNav variant="pills" className="flex-column">
+                    {headers.map((header, index) => (
+                      <CNavItem key={index}>
+                        <CNavLink
+                          href={`#${header}`}
+                          active={activeKey === index}
+                          onClick={(evt) => {
+                            evt.preventDefault();
+                            setActiveKey(index);
+                          }}
+                        >
+                          {capitalize(header)}
+                        </CNavLink>
+                      </CNavItem>
+                    ))}
+                  </CNav>
+                </CCol>
+                <CCol md={9}>
+                  <CTabContent>
+                    {(() => {
+                      const column = headers[activeKey];
 
-                    return (
-                      <CTabPane visible>
-                        <h2>{capitalize(column)}</h2>
-                        {timeColumn === column && (
-                          <div>
-                            <CBadge color="primary">Primary time column</CBadge>
-                          </div>
-                        )}
-                        <h3>Column data type</h3>
-                        {timeColumn === column && (
-                          <div>
-                            <CBadge color="warning">Must be datetime</CBadge>
-                          </div>
-                        )}
-                        <div>
-                          Current type (auto detect):{" "}
-                          {dataset
-                            .map((row: any) => typeof row[column])
-                            .filter(
-                              (value: any, index: number, self: any) =>
-                                self.indexOf(value) === index
-                            )
-                            .join(", ")}
-                        </div>
-                        <CFormSelect
-                          label="Data type"
-                          defaultValue={SELECT_STATE_NONE}
-                          onChange={(evt) => console.log(evt.target.value)}
-                          options={[
-                            {
-                              label: "Select data type for column",
-                              value: SELECT_STATE_NONE,
-                            },
-                            ...datatypes.map((datatype) => ({
-                              label: capitalize(datatype),
-                              value: datatype,
-                            })),
-                          ]}
-                        />
-                        <h3>Data series</h3>
-                        <CChartLine
-                          data={generateChartFormatForSeries(
-                            dataset.map((row: any) => row[timeColumn]),
-                            capitalize(column),
-                            dataset.map((row: any) => row[column]),
-                            chartColor
+                      return (
+                        <CTabPane visible>
+                          <h2>{capitalize(column)}</h2>
+                          {timeColumn === column && (
+                            <div>
+                              <CBadge color="primary">
+                                Primary time column
+                              </CBadge>
+                            </div>
                           )}
-                          type={"line"}
-                        />
-                        <h3>Value distribution</h3>
-                        {(() => {
-                          // TODO: create bins if data type allows
-                          const counts = {};
-                          dataset.forEach(
-                            (row: any) =>
-                              (counts[row[column]] = counts[row[column]]
-                                ? counts[row[column]] + 1
-                                : 1)
-                          );
-                          const keys = Object.keys(counts).sort();
-                          return (
-                            <CChartBar
-                              type="bar"
-                              data={{
-                                labels: keys,
-                                datasets: [
-                                  {
-                                    label: "Value distribution",
-                                    data: keys.map((key: any) => counts[key]),
-                                    backgroundColor: chartColor + "30",
-                                    borderColor: chartColor + "90",
-                                  },
-                                ],
-                              }}
-                            />
-                          );
-                        })()}
-                        <h3>Validation errors</h3>
-                        <h3>Output column name</h3>
-                        <CBadge color="warning">
-                          Possibly defined by feature, e.g. {`"ds" or "y"`}
-                        </CBadge>
-                      </CTabPane>
-                    );
-                  })()}
-                </CTabContent>
-              </CCol>
-            </CRow>
+                          <h3>Column data type</h3>
+                          {timeColumn === column && (
+                            <div>
+                              <CBadge color="warning">Must be datetime</CBadge>
+                            </div>
+                          )}
+                          <div>
+                            Current type (auto detect):{" "}
+                            {dataset
+                              .map((row: any) => typeof row[column])
+                              .filter(
+                                (value: any, index: number, self: any) =>
+                                  self.indexOf(value) === index
+                              )
+                              .join(", ")}
+                          </div>
+                          <CFormSelect
+                            label="Data type"
+                            defaultValue={SELECT_STATE_NONE}
+                            onChange={(evt) => console.log(evt.target.value)}
+                            options={[
+                              {
+                                label: "Select data type for column",
+                                value: SELECT_STATE_NONE,
+                              },
+                              ...datatypes.map((datatype) => ({
+                                label: capitalize(datatype),
+                                value: datatype,
+                              })),
+                            ]}
+                          />
+                          <h3>Data series</h3>
+                          <CChartLine
+                            data={generateChartFormatForSeries(
+                              dataset.map((row: any) => row[timeColumn]),
+                              capitalize(column),
+                              dataset.map((row: any) => row[column]),
+                              chartColor
+                            )}
+                            type={"line"}
+                          />
+                          <h3>Value distribution</h3>
+                          {(() => {
+                            // TODO: create bins if data type allows
+                            const counts = {};
+                            dataset.forEach(
+                              (row: any) =>
+                                (counts[row[column]] = counts[row[column]]
+                                  ? counts[row[column]] + 1
+                                  : 1)
+                            );
+                            const keys = Object.keys(counts).sort();
+                            return (
+                              <CChartBar
+                                type="bar"
+                                data={{
+                                  labels: keys,
+                                  datasets: [
+                                    {
+                                      label: "Value distribution",
+                                      data: keys.map((key: any) => counts[key]),
+                                      backgroundColor: chartColor + "30",
+                                      borderColor: chartColor + "90",
+                                    },
+                                  ],
+                                }}
+                              />
+                            );
+                          })()}
+                          <h3>Validation errors</h3>
+                          <h3>Output column name</h3>
+                          <CBadge color="warning">
+                            Possibly defined by feature, e.g. {`"ds" or "y"`}
+                          </CBadge>
+                        </CTabPane>
+                      );
+                    })()}
+                  </CTabContent>
+                </CCol>
+              </CRow>
+            </>
           )}
       </CContainer>
     </Layout>
