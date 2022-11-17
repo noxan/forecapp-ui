9117565911e50400080cc3edd3d439593da3d42d
Richard <richard@stromer.org>
2022-11-16 16:22:20 -0800

    transfer notes of model components from md file to ui

--- pages/model.tsx
+++ pages/model.tsx
@@ -35,7 +35,18 @@ export default function Debug() {
             <CCard className="mb-3">
               <CCardBody>
                 <CCardTitle>Trend</CCardTitle>
-                <CCardText>Text</CCardText>
+                <CCardText>
+                  T(t) = Trend at time t
+                  <ul>
+                    <li> growth = off | linear</li>
+                    <li>changepoints = [datetimes]</li>
+                    <li>n_changepoints = int</li>
+                    <li>changepoints_range = float</li>
+                    <li>trend_reg = float</li>
+                    <li>trend_reg_threshold = bool</li>
+                    <li>trend_global_local = global | local</li>
+                  </ul>
+                </CCardText>
               </CCardBody>
             </CCard>
           </CCol>
@@ -43,7 +54,17 @@ export default function Debug() {
             <CCard className="mb-3">
               <CCardBody>
                 <CCardTitle>Seasonality</CCardTitle>
-                <CCardText>Text</CCardText>
+                <CCardText>
+                  S(t) = Seasonal effects at time t
+                  <ul>
+                    <li>yearly_seasonality = bool | auto | int</li>
+                    <li>weekly_seasonality = bool | auto | int</li>
+                    <li>daily_seasonality = bool | auto | int</li>
+                    <li>seasonality_mode = additive | multiplicative</li>
+                    <li>seasonality_reg = float</li>
+                    <li>season_global_local = global | local</li>
+                  </ul>
+                </CCardText>
               </CCardBody>
             </CCard>
           </CCol>
@@ -51,7 +72,17 @@ export default function Debug() {
             <CCard className="mb-3">
               <CCardBody>
                 <CCardTitle>Auto regression</CCardTitle>
-                <CCardText>Text</CCardText>
+                <CCardText>
+                  A(t) = Auto-regression effects at time t based on past
+                  observations
+                  <ul>
+                    <li>n_lags = int</li>
+                    <li>ar_reg = float</li>
+                    <li>n_forecasts = int</li>
+                    <li>num_hidden_layers = int (default = 0)</li>
+                    <li>d_hidden = int</li>
+                  </ul>
+                </CCardText>
               </CCardBody>
             </CCard>
           </CCol>
@@ -59,7 +90,9 @@ export default function Debug() {
             <CCard className="mb-3">
               <CCardBody>
                 <CCardTitle>Events and holidays</CCardTitle>
-                <CCardText>Text</CCardText>
+                <CCardText>
+                  E(t) = Event and holiday effects at time t
+                </CCardText>
               </CCardBody>
             </CCard>
           </CCol>
@@ -67,7 +100,18 @@ export default function Debug() {
             <CCard className="mb-3">
               <CCardBody>
                 <CCardTitle>Future and lagged regressors</CCardTitle>
-                <CCardText>Text</CCardText>
+                <CCardText>
+                  <ul>
+                    <li>
+                      F(t) = Regression effects at time t for future-known
+                      exogenous variables
+                    </li>
+                    <li>
+                      L(t) = Regression effects at time t for lagged
+                      observations of exogenous variables
+                    </li>
+                  </ul>
+                </CCardText>
               </CCardBody>
             </CCard>
           </CCol>
@@ -75,7 +119,18 @@ export default function Debug() {
             <CCard className="mb-3">
               <CCardBody>
                 <CCardTitle>Training</CCardTitle>
-                <CCardText>Text</CCardText>
+                <CCardText>
+                  <ul>
+                    <li> learning_rate = float</li>
+                    <li>epochs = int</li>
+                    <li>batch_size = int</li>
+                    <li>newer_samples_weight = float</li>
+                    <li>newer_samples_start = float</li>
+                    <li>loss_func = str | torch.nn.functional.loss</li>
+                    <li>collect_metrics = bool | [str]</li>
+                    <li>quantiles = [float]</li>
+                  </ul>
+                </CCardText>
               </CCardBody>
             </CCard>
           </CCol>
@@ -83,7 +138,11 @@ export default function Debug() {
             <CCard className="mb-3">
               <CCardBody>
                 <CCardTitle>Validation</CCardTitle>
-                <CCardText>Text</CCardText>
+                <CCardText>
+                  <ul>
+                    <li>???</li>
+                  </ul>
+                </CCardText>
               </CCardBody>
             </CCard>
           </CCol>
@@ -91,7 +150,18 @@ export default function Debug() {
             <CCard className="mb-3">
               <CCardBody>
                 <CCardTitle>Data preparation</CCardTitle>
-                <CCardText>Text</CCardText>
+                <CCardText>
+                  <ul>
+                    <li>impute_missing = bool</li>
+                    <li>impute_linear = int</li>
+                    <li>impute_rolling = int</li>
+                    <li>drop_missing = bool</li>
+                    <li>normalize = str</li>
+                    <li>global_normalization = bool</li>
+                    <li>global_time_normalization = bool</li>
+                    <li>unknown_data_normalization = bool</li>
+                  </ul>
+                </CCardText>
               </CCardBody>
             </CCard>
           </CCol>
