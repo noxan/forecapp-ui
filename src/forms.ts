c6631f5b3c18711aa0cd46f54eb758d5afe2191a
Richard <richard@stromer.org>
2022-11-16 16:47:50 -0800

    add default values for trend model config

--- src/forms.ts
+++ src/forms.ts
@@ -3,17 +3,21 @@ import { RJSFSchema } from "@rjsf/utils";
 export const modelConfigSchema: RJSFSchema = {
   title: "Model trend",
   type: "object",
-  required: ["title"],
+  required: ["growth"],
   properties: {
-    growth: { type: "string", enum: ["off", "linear"] },
+    growth: { type: "string", enum: ["off", "linear"], default: "linear" },
     changepoints: {
       type: "array",
       items: { type: "string", format: "date-time" },
     },
-    n_changepoints: { type: "integer" },
-    changepoints_range: { type: "number" },
-    trend_reg: { type: "number" },
-    trend_reg_threshold: { type: "boolean" },
-    trend_global_local: { type: "string", enum: ["global", "local"] },
+    n_changepoints: { type: "integer", default: 10 },
+    changepoints_range: { type: "number", default: 0.8 },
+    trend_reg: { type: "number", default: 0 },
+    trend_reg_threshold: { type: "boolean", default: false },
+    trend_global_local: {
+      type: "string",
+      enum: ["global", "local"],
+      default: "global",
+    },
   },
 };
