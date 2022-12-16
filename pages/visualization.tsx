e460e3563ebea6c6d7437ce7287fdf1f2b5b67ed
Richard <richard@stromer.org>
2022-12-15 19:08:27 -0800

    make plotly the default visualization

--- pages/visualization.tsx
+++ pages/visualization.tsx
@@ -1,32 +1,59 @@
-import { CChartLine } from "@coreui/react-chartjs";
 import { CCol, CContainer, CRow } from "@coreui/react";
 import Layout from "../components/Layout";
 import { useAppSelector } from "../src/hooks";
-import { transformDatasetForChart } from "../src/helpers";
 import MissingDatasetPlaceholder from "../components/MissingDatasetPlaceholder";
+import dynamic from "next/dynamic";
+import { selectTargetColumn, selectTimeColumn } from "../src/store/selectors";
+import { isColumnValid } from "../src/definitions";
+import MissingColumnPlaceholder from "../components/MissingColumnPlaceholder";
+
+const PlotlyChart = dynamic(() => import("../components/PlotlyChart"), {
+  ssr: false,
+  loading: () => <>Loading...</>,
+});
 
 export default function Visualization() {
   const datasets = useAppSelector((state) => state.datasets);
+  const timeColumn = useAppSelector(selectTimeColumn);
+  const targetColumn = useAppSelector(selectTargetColumn);
+
+  const areColumnsValid =
+    isColumnValid(timeColumn) && isColumnValid(targetColumn);
 
   if (!datasets.raw) {
     return <MissingDatasetPlaceholder />;
   }
-
-  const chartData = transformDatasetForChart(datasets.raw);
+  if (!areColumnsValid) {
+    return (
+      <Layout>
+        <CContainer>
+          <CRow>
+            <CCol>
+              <MissingColumnPlaceholder />
+            </CCol>
+          </CRow>
+        </CContainer>
+      </Layout>
+    );
+  }
 
   return (
     <Layout>
       <CContainer>
         <CRow className="my-2">
           <CCol>
-            <h1>Visualization</h1>
+            <h1>Plotly</h1>
           </CCol>
         </CRow>
       </CContainer>
       <CContainer fluid>
         <CRow className="my-2">
           <CCol>
-            <CChartLine data={chartData} />
+            <PlotlyChart
+              dataset={datasets.raw}
+              timeColumn={timeColumn}
+              targetColumn={targetColumn}
+            />
           </CCol>
         </CRow>
       </CContainer>
