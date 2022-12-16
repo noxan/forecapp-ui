3682b72453ee710dce8c25ad7e07265346383546
Richard <richard@stromer.org>
2022-12-15 18:23:58 -0800

    load partial plotly lib and tweak legend

--- components/PlotlyChart.tsx
+++ components/PlotlyChart.tsx
@@ -1,4 +1,7 @@
-import Plot from "react-plotly.js";
+import createPlotComponent from "react-plotly.js/factory";
+
+const Plotly = require("plotly.js-gl2d-dist");
+const Plot = createPlotComponent(Plotly);
 import { capitalize } from "../src/helpers";
 
 export default function PlotlyChart({
@@ -26,6 +29,8 @@ export default function PlotlyChart({
       layout={{
         autosize: true,
         hovermode: "closest",
+        showlegend: true,
+        legend: { orientation: "h" },
       }}
       config={{
         responsive: true,
