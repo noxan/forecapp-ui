5e8a233f1828000cec12a0a283e736857dcdf466
Richard <richard@stromer.org>
2022-12-21 12:25:52 -0800

    remove sample text

--- components/prediction/Builder.tsx
+++ components/prediction/Builder.tsx
@@ -9,18 +9,6 @@ import { useAppDispatch, useAppSelector } from "../../src/hooks";
 import { editModelConfig } from "../../src/store/models";
 import { selectModelConfiguration } from "../../src/store/selectors";
 
-const placeholderText = (
-  <>
-    <strong>This is the first item&apos;s accordion body.</strong> It is hidden
-    by default, until the collapse plugin adds the appropriate classes that we
-    use to style each element. These classes control the overall appearance, as
-    well as the showing and hiding via CSS transitions. You can modify any of
-    this with custom CSS or overriding our default variables. It&apos;s also
-    worth noting that just about any HTML can go within the{" "}
-    <code>.accordion-body</code>, though the transition does limit overflow.
-  </>
-);
-
 const PredictionBuilder = () => {
   const modelConfiguration = useAppSelector(selectModelConfiguration);
   const dispatch = useAppDispatch();
