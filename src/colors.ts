f432e5dda0051be6d2acbaf702e6e07ca4445bb3
Richard <richard@stromer.org>
2022-12-22 17:51:03 -0800

    fine tune color palette

--- src/colors.ts
+++ src/colors.ts
@@ -1,25 +1,12 @@
-export const colors2 = [
-  "#1abc9c",
+export const colors = [
+  "#babebe",
+  "#f1c40f",
   "#2ecc71",
   "#3498db",
-  "#9b59b6",
-  "#34495e",
-  "#f1c40f",
   "#e67e22",
+  "#1f2435",
+  "#9b59b6",
+  "#62efcd",
   "#e74c3c",
-  "#ecf0f1",
-  "#95a5a6",
-];
-
-export const colors = [
-  "#f39c12",
-  "#16a085",
-  "#2c3e50",
-  "#c0392b",
-  "#bdc3c7",
-  "#27ae60",
-  "#2980b9",
-  "#8e44ad",
-  "#d35400",
-  "#7f8c8d",
+  "#c8db00",
 ];
