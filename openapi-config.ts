20cb013bf90c581e2174e4a0149a54a68c129884
Richard <richard@stromer.org>
2022-12-25 09:02:06 +0100

    get openapi code generation working

--- openapi-config.ts
+++ openapi-config.ts
@@ -1,7 +1,10 @@
 import type { ConfigFile } from "@rtk-query/codegen-openapi";
 
+const apiUrl = "https://api.forecapp.link";
+// TODO: make it work with process.env.NEXT_PUBLIC_API_URL
+
 const config: ConfigFile = {
-  schemaFile: "https://petstore3.swagger.io/api/v3/openapi.json",
+  schemaFile: `${apiUrl}/openapi.json`,
   apiFile: "./src/store/emptyApi.ts",
   apiImport: "emptySplitApi",
   outputFile: "./src/store/api.ts",
