f86524b501a323f42d8abf9b19b743242a1636fb
Richard <richard@stromer.org>
2022-12-25 08:56:55 +0100

    remove outdated api redirect (via nextjs)

--- next.config.js
+++ next.config.js
@@ -2,15 +2,6 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-  async rewrites() {
-    return [
-      {
-        source: "/api-production/:slug",
-        destination:
-          "http://ec2-54-219-210-155.us-west-1.compute.amazonaws.com/:slug",
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
