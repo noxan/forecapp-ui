0aa51a3b89caca37092a3398a5227fe58bfc016f
Richard <richard@stromer.org>
2022-12-27 16:48:51 +0100

    remove check for time column

--- components/dataset/DatasetColumns.tsx
+++ components/dataset/DatasetColumns.tsx
@@ -29,43 +29,39 @@ const DatasetColumns = () => {
 
   return (
     <CContainer fluid>
-      {isColumnValid(timeColumn) && (
-        <>
-          <CRow className="my-2">
-            <CCol>
-              <h2>Columns explorer and configurations</h2>
-            </CCol>
-          </CRow>
-          <CRow className="my-2">
-            <CCol md={3}>
-              <CNav variant="pills" className="flex-column">
-                {columns.map((header, index) => (
-                  <CNavItem key={index}>
-                    <CNavLink
-                      href={`#${header}`}
-                      active={activeKey === index}
-                      onClick={(evt) => {
-                        evt.preventDefault();
-                        setActiveKey(index);
-                      }}
-                    >
-                      {capitalize(header)}
-                    </CNavLink>
-                  </CNavItem>
-                ))}
-              </CNav>
-            </CCol>
-            <CCol md={9}>
-              <ColumnConfigPanel
-                column={activeColumn}
-                dataset={dataset}
-                timeColumn={timeColumn}
-                targetColumn={targetColumn}
-              />
-            </CCol>
-          </CRow>
-        </>
-      )}
+      <CRow className="my-2">
+        <CCol>
+          <h2>Columns explorer and configurations</h2>
+        </CCol>
+      </CRow>
+      <CRow className="my-2">
+        <CCol md={3}>
+          <CNav variant="pills" className="flex-column">
+            {columns.map((header, index) => (
+              <CNavItem key={index}>
+                <CNavLink
+                  href={`#${header}`}
+                  active={activeKey === index}
+                  onClick={(evt) => {
+                    evt.preventDefault();
+                    setActiveKey(index);
+                  }}
+                >
+                  {capitalize(header)}
+                </CNavLink>
+              </CNavItem>
+            ))}
+          </CNav>
+        </CCol>
+        <CCol md={9}>
+          <ColumnConfigPanel
+            column={activeColumn}
+            dataset={dataset}
+            timeColumn={timeColumn}
+            targetColumn={targetColumn}
+          />
+        </CCol>
+      </CRow>
     </CContainer>
   );
 };
