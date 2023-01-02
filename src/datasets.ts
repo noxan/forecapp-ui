8f8d31a688ffcb0720a9d63b64d0c9de42adaa3b
Richard <richard@stromer.org>
2023-01-02 10:57:20 +0100

    add description to energy dataset

--- src/datasets.ts
+++ src/datasets.ts
@@ -4,11 +4,15 @@ const datasetExamples = [
   {
     title: "Energy (2018)",
     filename: "energy_dataset_2018.csv",
+    description:
+      "This dataset contains 1 year of electrical consumption, generation, pricing, and weather data for Spain in hourly resolution.",
     image: "photo-1508791290064-c27cc1ef7a9a",
   },
   {
     title: "Energy (full)",
     filename: "energy_dataset.csv",
+    description:
+      "This dataset contains 4 years of electrical consumption, generation, pricing, and weather data for Spain in hourly resolution.",
     image: "photo-1473341304170-971dccb5ac1e",
   },
   {
@@ -24,6 +28,8 @@ const datasetExamples = [
   {
     title: "Energy (small)",
     filename: "energy_dataset_small.csv",
+    description:
+      "This dataset contains 1 month of electrical consumption, generation, pricing, and weather data for Spain in hourly resolution.",
     image: "photo-1473341304170-971dccb5ac1e",
   },
 ].map((dataset) => ({
