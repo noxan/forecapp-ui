fe49109d0c0a944e5903935063af4d0564fd5450
Richard <richard@stromer.org>
2023-01-09 15:45:37 +0100

    refactor info box

--- components/Info.tsx
+++ components/Info.tsx
@@ -3,15 +3,13 @@ import Image from "next/image";
 import { ReactNode } from "react";
 
 const Info = ({
-  text,
   children,
   placement,
 }: {
-  text: string | ReactNode;
   children?: ReactNode;
   placement?: "right" | "auto" | "top" | "bottom" | "left" | undefined;
 }) => (
-  <CTooltip content={children || text} placement={placement || "right"}>
+  <CTooltip content={children} placement={placement || "right"}>
     <a>
       <Image src="/icon-info.svg" width="30" height="30" alt="Help text" />
     </a>
--- components/prediction/Builder.tsx
+++ components/prediction/Builder.tsx
@@ -70,7 +70,13 @@ const PredictionBuilder = () => {
             }
           />
           Number of changepoints{" "}
-          <Info text="Trend change points, also known as inflection points, are points in a data series where the direction of the trend changes. For example, if a data series is trending upwards and then begins to trend downwards, the point where the upward trend ends and the downward trend begins is a trend change point." />
+          <Info>
+            Trend change points, also known as inflection points, are points in
+            a data series where the direction of the trend changes. For example,
+            if a data series is trending upwards and then begins to trend
+            downwards, the point where the upward trend ends and the downward
+            trend begins is a trend change point.
+          </Info>
           <CFormInput
             type="number"
             min={0}
@@ -136,7 +142,19 @@ const PredictionBuilder = () => {
         <CAccordionHeader>Auto regression</CAccordionHeader>
         <CAccordionBody>
           How many previous values should be used to predict the next value?
-          <Info text="Autoregression uses previous values of a time-series to predict the value of the next time point. It is based on the assumption that the past values of a time-series are related to the future values in a linear way. The optimal value for an autoregressive model is determined by the complexity of the relationship between the past and future values in the time-series data. A larger value may capture more of this complexity, but it may also increase the risk of overfitting, where the model fits the training data too closely and may not generalize well to new data. On the other hand, a smaller value may not capture enough complexity and may result in a model with lower accuracy." />
+          <Info>
+            Autoregression uses previous values of a time-series to predict the
+            value of the next time point. It is based on the assumption that the
+            past values of a time-series are related to the future values in a
+            linear way. The optimal value for an autoregressive model is
+            determined by the complexity of the relationship between the past
+            and future values in the time-series data. A larger value may
+            capture more of this complexity, but it may also increase the risk
+            of overfitting, where the model fits the training data too closely
+            and may not generalize well to new data. On the other hand, a
+            smaller value may not capture enough complexity and may result in a
+            model with lower accuracy.
+          </Info>
           <CFormInput
             className="mb-4"
             type="number"
