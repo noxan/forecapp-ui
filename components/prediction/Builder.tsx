2e67a810e7a45fdcb847a6c2d854ae31c0512bce
Richard <richard@stromer.org>
2023-01-09 13:57:39 +0100

    add info box for trend changepoints

--- components/prediction/Builder.tsx
+++ components/prediction/Builder.tsx
@@ -69,7 +69,8 @@ const PredictionBuilder = () => {
               )
             }
           />
-          Number of changepoints
+          Number of changepoints{" "}
+          <Info text="Trend change points, also known as inflection points, are points in a data series where the direction of the trend changes. For example, if a data series is trending upwards and then begins to trend downwards, the point where the upward trend ends and the downward trend begins is a trend change point." />
           <CFormInput
             type="number"
             min={0}
