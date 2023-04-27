6ff2fd32e94c9c11fb97d0efc9c677942a64ba5e
Peter Zaika <125574974+pzaika@users.noreply.github.com>
2023-04-27 09:13:31 +0100

    Added better comments.

--- src/error.ts
+++ src/error.ts
@@ -1,18 +1,16 @@
-/*
-  Error Types
-*/
-
-// Represents an error in connecting the backend
+// Typing for an error in connecting to the server
 export type HTTPError = {
   message: string;
   name: string;
   stack: string;
 };
 
+// Typing for the response sent by the server when NeuralProphet has an error
 export type NeuralProphetError = {
   detail: string;
 };
 
+// Typing for FastAPI not being able to convert the POSTed data into a valid configuration
 export type ValidationError = {
   detail: {
     ctx: { limit_value: number };
