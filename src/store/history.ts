41b6f90bff70b828108861b0bef671b3de295030
Peter Zaika <125574974+pzaika@users.noreply.github.com>
2023-05-11 10:25:34 +0100

    Remove unnecessary import

--- src/store/history.ts
+++ src/store/history.ts
@@ -1,4 +1,4 @@
-import { createSlice, current } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import { ModelState } from "./models";
 import { apiPrediction } from "./datasets";
 
