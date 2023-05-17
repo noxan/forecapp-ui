38f3ba03d3d37c7fd93e637f83e7a48f113023f8
Peter Zaika <125574974+pzaika@users.noreply.github.com>
2023-05-17 09:36:55 +0100

    Changed prediction chart to NP standard

--- components/prediction/Chart.tsx
+++ components/prediction/Chart.tsx
@@ -17,12 +17,70 @@ const renameColumn = (column: string) =>
 const transformPredictionData = (forecast: any): Plotly.Data[] => {
   const columnHeaders = Object.keys(forecast).filter((item) => item !== "ds");
   const x = Object.values(forecast.ds);
+  const quantiles = columnHeaders.filter((colName) => {
+    return colName.at(colName.length - 1) === "%";
+  });
+  const res: Plotly.Data[] = [];
 
-  const res = columnHeaders.map((columnHeader) => ({
+  if (quantiles.length > 1) {
+    res.push({
+      type: "scattergl",
+      mode: "lines",
+      name: "95% Confidence Interval",
+      fillcolor: "rgba(45, 146, 255, 0.2)",
+      fill: "tonexty",
+      line: { color: "rgba(45, 146, 255, 0.2)", width: 1 },
+      y: Object.values(forecast[quantiles[0]]),
+      x,
+    } as Plotly.Data);
+    for (let i = 1; i < quantiles.length; i++) {
+      res.push({
+        type: "scattergl",
+        mode: "lines",
+        showlegend: false,
+        fillcolor: "rgba(45, 146, 255, 0.2)",
+        fill: "tonexty",
+        line: { color: "rgba(45, 146, 255, 0.2)", width: 1 },
+        y: Object.values(forecast[quantiles[i]]),
+        x,
+      } as Plotly.Data);
+    }
+  }
+
+  res.push({
     type: "scattergl",
     mode: "lines",
-    name: capitalize(renameColumn(columnHeader)),
-    y: Object.values(forecast[columnHeader]),
+    line: { color: "#2d92ff", width: 2 },
+    name: capitalize(renameColumn("yhat1")),
+    y: Object.values(forecast["yhat1"]),
+    x: x,
+  } as Plotly.Data);
+
+  res.push({
+    type: "scattergl",
+    mode: "markers",
+    marker: { color: "black", size: 4 },
+    name: capitalize(renameColumn("y")),
+    y: Object.values(forecast["y"]),
+    x,
+  } as Plotly.Data);
+
+  return res;
+};
+
+const transformSeasonalityData = (forecast: any): Plotly.Data[] => {
+  const columnHeaders = Object.keys(forecast).filter((item) => item !== "ds");
+  const x = Object.values(forecast.ds);
+  const seasonalityHeaders = columnHeaders.filter((columnName) => {
+    return columnName.startsWith("season");
+  });
+
+  const res = seasonalityHeaders.map((colName) => ({
+    type: "scattergl",
+    mode: "lines",
+    line: { color: "#2d92ff", width: 2, shape: "spline", smoothing: 1 },
+    name: capitalize(renameColumn(colName)),
+    y: Object.values(forecast[colName]),
     x,
   })) as Plotly.Data[];
 
@@ -57,29 +115,54 @@ const PredictionChart = ({
   predictionData: any;
   forecasts: number | undefined;
 }) => (
-  <PlotlyChart
-    useResizeHandler
-    data={transformPredictionData(predictionData.forecast)}
-    layout={{
-      hovermode: "x",
-      showlegend: true,
-      legend: { orientation: "h", y: -0.05 },
-      margin: { t: 10, r: 30 }, // b: 10, l: 30, pad: 10
-      shapes: [
-        generateHistoryMarker(
-          Object.values(predictionData.forecast.ds),
-          forecasts
-        ),
-      ],
-      yaxis: {
-        title: { text: targetColumn },
-      },
-    }}
-    config={{
-      responsive: true,
-    }}
-    style={{ width: "100%", minHeight: "85vh" }}
-  />
+  <>
+    <PlotlyChart
+      useResizeHandler
+      data={transformPredictionData(predictionData.forecast)}
+      layout={{
+        hovermode: "x",
+        showlegend: true,
+        legend: { orientation: "h", y: -0.05 },
+        margin: { t: 10, r: 30 }, // b: 10, l: 30, pad: 10
+        shapes: [
+          generateHistoryMarker(
+            Object.values(predictionData.forecast.ds),
+            forecasts
+          ),
+        ],
+        yaxis: {
+          title: { text: targetColumn },
+        },
+      }}
+      config={{
+        responsive: true,
+      }}
+      style={{ width: "100%", minHeight: "85vh" }}
+    />
+    <PlotlyChart
+      useResizeHandler
+      data={transformSeasonalityData(predictionData.forecast)}
+      layout={{
+        hovermode: "x",
+        showlegend: true,
+        legend: { orientation: "h", y: -0.05 },
+        margin: { t: 10, r: 30 }, // b: 10, l: 30, pad: 10
+        shapes: [
+          generateHistoryMarker(
+            Object.values(predictionData.forecast.ds),
+            forecasts
+          ),
+        ],
+        yaxis: {
+          title: { text: targetColumn },
+        },
+      }}
+      config={{
+        responsive: true,
+      }}
+      style={{ width: "100%", minHeight: "85vh" }}
+    />
+  </>
 );
 
 export default PredictionChart;
