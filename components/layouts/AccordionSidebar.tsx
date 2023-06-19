a812a3cb54b78bb66264fecb0598eff31f68769c
Peter Zaika <125574974+pzaika@users.noreply.github.com>
2023-06-19 11:41:38 -0400

    Redesigned sidebar

--- components/layouts/AccordionSidebar.tsx
+++ components/layouts/AccordionSidebar.tsx
@@ -4,34 +4,85 @@ import {
   CAccordionHeader,
   CAccordionItem,
   CButton,
+  CNavGroup,
+  CNavItem,
+  CNavLink,
+  CSidebar,
+  CSidebarNav,
 } from "@coreui/react";
+import HomeIcon from "@mui/icons-material/Home";
+import { ReactElement } from "react";
 
-export default function AccordionSidebar() {
+export type AccordionSideBarProps = {
+  content: AccordionSideBarGroupProps[];
+  activePageInd: number;
+  activeSubPageInd: number;
+  onNavClick: (
+    pageInd: number,
+    subPageInd: number,
+    event: React.MouseEvent<HTMLElement>
+  ) => void;
+};
+
+export type AccordionSideBarGroupProps = {
+  pageName: string;
+  subPages: (string | ReactElement)[];
+};
+
+export default function AccordionSideBar(props: AccordionSideBarProps) {
   return (
-    <div>
-      <CButton>Home</CButton>
-      <CAccordion activeItemKey={1}>
-        <CAccordionItem itemKey={1}>
-          <CAccordionHeader>
-            <CButton onClick={() => console.log("Test")}>Test</CButton>
-          </CAccordionHeader>
-          <CAccordionBody>
-            <p>Test</p>
-          </CAccordionBody>
-        </CAccordionItem>
-        <CAccordionItem onClick={() => console.log("Test")} itemKey={2}>
-          <CAccordionHeader>Test</CAccordionHeader>
-          <CAccordionBody>
-            <p>Test</p>
-          </CAccordionBody>
-        </CAccordionItem>
-        <CAccordionItem itemKey={3}>
-          <CAccordionHeader>Test</CAccordionHeader>
-          <CAccordionBody>
-            <p>Test</p>
-          </CAccordionBody>
-        </CAccordionItem>
-      </CAccordion>
-    </div>
+    <CSidebar position="sticky">
+      <CSidebarNav>
+        <CNavItem href="/">
+          <HomeIcon
+            sx={{
+              width: "2em",
+              alignContent: "center",
+              justifyContent: "flex-start",
+              display: "flex",
+            }}
+          />
+          Home
+        </CNavItem>
+        {props.content.map((sideBarGroup, pageInd) => {
+          if (sideBarGroup.subPages.length === 0) {
+            return (
+              <CNavItem key={pageInd}>
+                <CNavLink
+                  active={props.activePageInd === pageInd}
+                  onClick={(event) => props.onNavClick(pageInd, -1, event)}
+                >
+                  {sideBarGroup.pageName}
+                </CNavLink>
+              </CNavItem>
+            );
+          }
+          return (
+            <CNavGroup toggler={sideBarGroup.pageName} key={pageInd}>
+              {sideBarGroup.subPages.map((subPage, subPageInd) => {
+                if (typeof subPage === "string") {
+                  return (
+                    <CNavItem key={subPageInd}>
+                      <CNavLink
+                        active={
+                          props.activePageInd === pageInd &&
+                          props.activeSubPageInd === subPageInd
+                        }
+                        onClick={(event) =>
+                          props.onNavClick(pageInd, subPageInd, event)
+                        }
+                      >
+                        {subPage}
+                      </CNavLink>
+                    </CNavItem>
+                  );
+                }
+                return subPage;
+              })}
+            </CNavGroup>
+          );
+        })}
+      </CSidebarNav>
+    </CSidebar>
   );
 }
