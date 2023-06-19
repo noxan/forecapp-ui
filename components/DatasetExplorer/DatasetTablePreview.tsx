81c94f80d1933e973542880434cb2cb56d1da917
Kaktus500 <delseith.paul@gmail.com>
2023-06-19 20:30:29 +0200

    Added Dataset Explorer

--- components/DatasetExplorer/DatasetTablePreview.tsx
+++ components/DatasetExplorer/DatasetTablePreview.tsx
@@ -1,52 +1,22 @@
-import { useState } from "react";
 import { CButton, CButtonGroup, CCol, CContainer, CRow } from "@coreui/react";
 import { useAppSelector } from "../../src/hooks";
 import { useAppDispatch } from "../../src/hooks";
 import { selectDataset } from "../../src/store/selectors";
+import { setTimeColumn, setTargetColumn } from "../../src/store/datasets";
 import Table from "../Table";
 
 const DatasetTablePreview = ({
-  timeColumn,
-  setTimeColumn,
-  targetColumn,
-  setTargetColumn,
+  selectTime,
+  selectTarget,
 }: {
-  timeColumn: string;
-  setTimeColumn: Function;
-  targetColumn: string;
-  setTargetColumn: Function;
+  selectTime: boolean;
+  selectTarget: boolean;
 }) => {
-  const [selectTime, setSelectTime] = useState<boolean>(false);
-  const [selectTarget, setSelectTarget] = useState<boolean>(false);
   const dataset = useAppSelector(selectDataset);
   const tableHead = dataset.slice(0, 10);
   const dispatch = useAppDispatch();
   return (
     <div>
-      <CButtonGroup>
-        <CButton
-          onClick={() => {
-            setSelectTime(true);
-            setSelectTarget(false);
-          }}
-          color="info"
-          variant={selectTime ? "outline" : undefined}
-          shape="rounded-0"
-        >
-          Set Time Column
-        </CButton>
-        <CButton
-          onClick={() => {
-            setSelectTime(false);
-            setSelectTarget(true);
-          }}
-          color="success"
-          variant={selectTarget ? "outline" : undefined}
-          shape="rounded-0"
-        >
-          Set Target Column
-        </CButton>
-      </CButtonGroup>
       <CRow className="my-2">
         <CCol
           onClick={(e: any) =>
