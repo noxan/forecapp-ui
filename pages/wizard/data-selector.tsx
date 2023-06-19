af6e443540eab8064bd6ec46a5e367ece8c317d8
Kaktus500 <delseith.paul@gmail.com>
2023-06-19 20:30:29 +0200

    Added Dataset Explorer

--- pages/wizard/data-selector.tsx
+++ pages/wizard/data-selector.tsx
@@ -1,23 +1,54 @@
+import { useState } from "react";
 import DatasetTablePreview from "../../components/DatasetExplorer/DatasetTablePreview";
 import DatasetVisualize from "../../components/DatasetExplorer/DatasetVisualize";
-import { useAppSelector } from "../../src/hooks";
-import { setTimeColumn, setTargetColumn } from "../../src/store/datasets";
-import {
-  selectTimeColumn,
-  selectTargetColumn,
-} from "../../src/store/selectors";
+import { CButton, CButtonGroup, CButtonToolbar } from "@coreui/react";
+import DatasetExplorer from "../../components/DatasetExplorer";
 
 export default function WizardDataSelectorPage() {
-  const timeColumn = useAppSelector(selectTimeColumn);
-  const targetColumn = useAppSelector(selectTargetColumn);
+  const [datasetExplorerVisible, setDatasetExplorerVisible] = useState(false);
+  const [selectTime, setSelectTime] = useState<boolean>(false);
+  const [selectTarget, setSelectTarget] = useState<boolean>(false);
   return (
     <div className="row">
       <div className="col-12 justify-content-start">
+        <CButtonToolbar role="group">
+          <CButtonGroup>
+            <CButton
+              onClick={() => {
+                setSelectTime(true);
+                setSelectTarget(false);
+              }}
+              color="info"
+              variant={selectTime ? "outline" : undefined}
+              shape="rounded-0"
+            >
+              Set Time Column
+            </CButton>
+            <CButton
+              onClick={() => {
+                setSelectTime(false);
+                setSelectTarget(true);
+              }}
+              color="success"
+              variant={selectTarget ? "outline" : undefined}
+              shape="rounded-0"
+            >
+              Set Target Column
+            </CButton>
+          </CButtonGroup>
+          <CButtonGroup>
+            <CButton onClick={() => setDatasetExplorerVisible(true)}>
+              Explore full dataset
+            </CButton>
+          </CButtonGroup>
+        </CButtonToolbar>
+        <DatasetExplorer
+          modalVisible={datasetExplorerVisible}
+          setModalVisible={setDatasetExplorerVisible}
+        />
         <DatasetTablePreview
-          timeColumn={timeColumn}
-          setTimeColumn={setTimeColumn}
-          targetColumn={targetColumn}
-          setTargetColumn={setTargetColumn}
+          selectTime={selectTime}
+          selectTarget={selectTarget}
         ></DatasetTablePreview>
         <DatasetVisualize />
       </div>
