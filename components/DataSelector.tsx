bd976bead32e8daaf86f78db5f835954abcd1530
Kaktus500 <delseith.paul@gmail.com>
2023-06-25 15:07:19 +0200

    Added time and target validation, missing disabling of next button

--- components/DataSelector.tsx
+++ components/DataSelector.tsx
@@ -1,11 +1,25 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import DatasetTablePreview from "./DatasetExplorer/DatasetTablePreview";
 import DatasetVisualize from "./DatasetExplorer/DatasetVisualize";
-import { CButton, CButtonGroup, CButtonToolbar } from "@coreui/react";
+import { CButton, CButtonGroup, CButtonToolbar, CAlert } from "@coreui/react";
+import { isColumnValid } from "../src/definitions";
+import { useAppSelector } from "../src/hooks";
+import { selectDataset, selectTimeColumn } from "../src/store/selectors";
+import MissingDatasetPlaceholder from "./MissingDatasetPlaceholder";
+import { isColumnDateTime } from "../src/data-validator";
 
 export default function DataSelector() {
   const [selectTime, setSelectTime] = useState<boolean>(false);
   const [selectTarget, setSelectTarget] = useState<boolean>(false);
+  const [columnWarningVisible, setColumnWarningVisible] =
+    useState<boolean>(false);
+  const timeColumn = useAppSelector(selectTimeColumn);
+  console.log(timeColumn);
+  const dataset = useAppSelector(selectDataset);
+
+  if (!dataset) {
+    return <MissingDatasetPlaceholder />;
+  }
   return (
     <>
       <CButtonToolbar role="group">
--- components/DatasetExplorer/DatasetTablePreview.tsx
+++ components/DatasetExplorer/DatasetTablePreview.tsx
@@ -1,9 +1,20 @@
-import { CButton, CButtonGroup, CCol, CContainer, CRow } from "@coreui/react";
+import { useState, useMemo } from "react";
+import {
+  CButton,
+  CButtonGroup,
+  CCol,
+  CContainer,
+  CRow,
+  CAlert,
+} from "@coreui/react";
 import { useAppSelector } from "../../src/hooks";
 import { useAppDispatch } from "../../src/hooks";
-import { selectDataset } from "../../src/store/selectors";
+import { selectDataset, selectTargetColumn } from "../../src/store/selectors";
 import { setTimeColumn, setTargetColumn } from "../../src/store/datasets";
+import { selectTimeColumn } from "../../src/store/selectors";
 import Table from "../Table";
+import { isColumnValid } from "../../src/definitions";
+import { isColumnDateTime } from "../../src/data-validator";
 
 const DatasetTablePreview = ({
   selectTime,
@@ -14,9 +25,31 @@ const DatasetTablePreview = ({
 }) => {
   const dataset = useAppSelector(selectDataset);
   const tableHead = dataset.slice(0, 10);
+  const [columnWarningVisible, setColumnWarningVisible] =
+    useState<boolean>(false);
   const dispatch = useAppDispatch();
+  const timeColumn = useAppSelector(selectTimeColumn);
+  const targetColumn = useAppSelector(selectTargetColumn);
+  console.log(timeColumn);
+  const isValidTime = isColumnValid(timeColumn);
+  const isValidTarget = isColumnValid(timeColumn);
+  const isTimeColumn = useMemo(() => {
+    if (!isColumnValid(timeColumn)) return true;
+    const res = isColumnDateTime(dataset, timeColumn);
+    !res ? setColumnWarningVisible(true) : setColumnWarningVisible(false);
+    return res;
+  }, [dataset, timeColumn]);
   return (
     <div>
+      <CAlert
+        dismissible
+        color="danger"
+        visible={columnWarningVisible}
+        onClose={() => setColumnWarningVisible(false)}
+      >
+        The selected column isn&apos;t a valid time column. Choose another, or
+        try editing the dataset!
+      </CAlert>
       <CRow className="my-2">
         <CCol
           onClick={(e: any) =>
