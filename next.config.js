e6d6f6f8c72763b8592c4ba5d156b80818e38751
Kaktus500 <delseith.paul@gmail.com>
2023-09-10 21:49:03 +0200

    Added config to import .svg graphics

--- next.config.js
+++ next.config.js
@@ -1,12 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   reactStrictMode: false,
   swcMinify: true,
   images: {
     unoptimized: true,
   },
   assetPrefix: ".",
+  webpack(
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
